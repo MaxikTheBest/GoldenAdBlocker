ce038db88f73e4da9f792aa1e479936814017d0a
MaxikTheBEst <maxik.uhnak@gmail.com>
2025-08-20 20:07:13 +0200

    Update download.user.js

--- goldenadblocker/download.user.js
+++ goldenadblocker/download.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         GoldenAdblocker
 // @namespace    https://ilomero.com/
-// @version      1.2.3
-// @description  Removes 99% of ads from YouTube :) auto-loading the latest version.
-// @author       Ilomero.com inspired by goodtube!
+// @version      1.2.4
+// @description  Removes 99% of ads from YouTube :) auto-loading the latest version. (added v5 loader, that is much better and faster than v4 and more stable)
+// @author       Ilomero.com
 // @updateURL    https://ilomero.com/goldenadblocker/download.user.js
 // @downloadURL  https://ilomero.com/goldenadblocker/download.user.js
 // @match        *://m.youtube.com/*
